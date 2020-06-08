4faf5eae612ee8996c7c7fc5240e362169200e15
taxijjang <gw9122@naver.com>
2020-06-08 17:08:26 +0900

    1일차

---
+++ instagram/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Post(models.Model):
+    message = models.TextField()
+    photo = models.ImageField(blank=True,upload_to="instagram/post/%y/%m/%d")
+    is_public = models.BooleanField(default=False, verbose_name="공개여부")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.message
+
+
